45f385f8305f73a3ed5f23be86459e66b0185ad9
m05e5 <calebmoses1111@gmail.com>
2021-08-23 15:21:18 +0100

    add styling

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import styles from "./TodoItem.module.css"
 const TodoItem = (props) => {
-
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#595959",
+    opacity: 0.4,
+    textDecoration: "line-through",
+  }
   return( 
-    <li>
+    <li className={styles.item}>
       <input 
         type="checkbox"
+        className={styles.checkbox}
         checked={props.todo.completed}
         onChange={() => props.handleChangeProps(props.todo.id)}
       />
-      {props.todo.title}
+      <span style={props.todo.completed ? completedStyle : null}>
+        {props.todo.title}
+      </span>
       <button onClick={() => props.delTodoProps(props.todo.id)}>Delete</button>
     </li>
   );
